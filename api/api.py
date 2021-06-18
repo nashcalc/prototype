8eff96223f8b7f95da6bae148ae0071594037437
Benjamin Shapiro <bps78@cornell.edu>
2021-06-18 11:54:22 -0400

    basic flask backend complete

--- api/api.py
+++ api/api.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+if __name__ == "__main__":
+    app.run()
--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Grid from "./components/grid.js";
-import Header from "./components/Header.js";
 import React from "react";
 import Button from "@material-ui/core/Button";
 
 function App() {
   const [rows, setRows] = React.useState(2);
   const [cols, setCols] = React.useState(2);
+
+  const [currentTime, setCurrentTime] = React.useState(0);
+
+  React.useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
 
   //const [forms, setForms] =
 
@@ -27,6 +34,7 @@ function App() {
           <Button onClick={() => setRows(rows + 1)}>+rows</Button>
           <Button onClick={() => setRows(rows - 1)}>-rows</Button>
         </div>
+        <p>The current time is {currentTime}.</p>
       </div>
     );
   } else if (rows > 1 && cols <= 1) {
