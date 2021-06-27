017f5161ce9c31efbaa6d781fa8371afe8a9cbdc
Ben <bps78@cornell.edu>
2021-06-26 22:53:38 -0400

    once again

--- api/app.py
+++ api/app.py
@@ -6,15 +6,6 @@ from solvenasheq import solvefornasheq
 app = Flask(__name__, static_folder="../build", static_url_path="")
 CORS(app)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return app.send_static_file('../public/index.html')
-
-@app.errorhandler(404)
-def not_found(e):
-  return app.send_static_file('../public/index.html')
-
 @app.route('/test', methods=['POST'])
 def postTest():
     data = request.get_json()
